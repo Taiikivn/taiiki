f661640bcfa5fb59254eb11f54ae947045b540c1
thuat <tk30ofmusic@gmail.com>
2021-11-14 19:19:05 +0700

    update

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -65,53 +65,6 @@ let getWebhook = (req, res) => {
     }
 };
 
-// Handles messages events
-// function handleMessage(sender_psid, received_message) {
-//     let response;
-//
-//     // Check if the message contains text
-//     if (received_message.text) {
-//
-//         // Create the payload for a basic text message
-//         response = {
-//             "text": `You sent the message: "${received_message.text}". Now send me an image!`
-//         }
-//     } else if (received_message.attachments) {
-//
-//     // Gets the URL of the message attachment
-//     let attachment_url = received_message.attachments[0].payload.url;
-//         response = {
-//             "attachment": {
-//                 "type": "template",
-//                 "payload": {
-//                     "template_type": "generic",
-//                     "elements": [{
-//                         "title": "Is this the right picture?",
-//                         "subtitle": "Tap a button to answer.",
-//                         "image_url": attachment_url,
-//                         "buttons": [
-//                             {
-//                                 "type": "postback",
-//                                 "title": "Yes!",
-//                                 "payload": "yes",
-//                             },
-//                             {
-//                                 "type": "postback",
-//                                 "title": "No!",
-//                                 "payload": "no",
-//                             }
-//                         ],
-//                     }]
-//                 }
-//             }
-//         }
-//
-// }
-//
-// // Sends the response message
-//     callSendAPI(sender_psid, response);
-// }
-
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
     let response;
@@ -158,48 +111,48 @@ function callSendAPI(sender_psid, response) {
 //     return nlp && nlp.entities && nlp.entities[name] && nlp.entities[name][0];
 // }
 
-// function firstTrait(nlp, name) {
-//     return nlp && nlp.entities && nlp.traits[name] && nlp.traits[name][0];
-// }
+function firstTrait(nlp, name) {
+    return nlp && nlp.entities && nlp.traits[name] && nlp.traits[name][0];
+}
 
-// function handleMessage(sender_psid, message) {
-//     //handle message for react, like press like button
-//     // id like button: sticker_id 369239263222822
+function handleMessage(sender_psid, message) {
+    //handle message for react, like press like button
+    // id like button: sticker_id 369239263222822
 
-//     if( message && message.attachments && message.attachments[0].payload){
-//         callSendAPI(sender_psid, "Thank you for watching my video !!!");
-//         // callSendAPIWithTemplate(sender_psid);
-//         callSendAPI(sender_psid,"Provjp chưa")
-//         return;
-//     }
+    if( message && message.attachments && message.attachments[0].payload){
+        callSendAPI(sender_psid, "Thank you for watching my video !!!");
+        // callSendAPIWithTemplate(sender_psid);
+        callSendAPI(sender_psid,"Provjp chưa")
+        return;
+    }
 
-//     let entitiesArr = [ "wit$greetings", "wit$thanks", "wit$bye" ];
-//     let entityChosen = "";
-//     entitiesArr.forEach((name) => {
-//         let entity = firstTrait(message.nlp, name);
-//         if (entity && entity.confidence > 0.8) {
-//             entityChosen = name;
-//         }
-//     });
+    let entitiesArr = [ "wit$greetings", "wit$thanks", "wit$bye" ];
+    let entityChosen = "";
+    entitiesArr.forEach((name) => {
+        let entity = firstTrait(message.nlp, name);
+        if (entity && entity.confidence > 0.8) {
+            entityChosen = name;
+        }
+    });
 
-//     if(entityChosen === ""){
-//         //default
-//         callSendAPI(sender_psid,`The bot is needed more training, try to say "thanks a lot" or "hi" to the bot` );
-//     }else{
-//        if(entityChosen === "wit$greetings"){
-//            //send greetings message
-//            callSendAPI(sender_psid,'Hi there! This bot is created by Hary Pham. Watch more videos on HaryPhamDev Channel!');
-//        }
-//        if(entityChosen === "wit$thanks"){
-//            //send thanks message
-//            callSendAPI(sender_psid,`You 're welcome!`);
-//        }
-//         if(entityChosen === "wit$bye"){
-//             //send bye message
-//             callSendAPI(sender_psid,'bye-bye!');
-//         }
-//     }
-// }
+    if(entityChosen === ""){
+        //default
+        callSendAPI(sender_psid,`The bot is needed more training, try to say "thanks a lot" or "hi" to the bot` );
+    }else{
+       if(entityChosen === "wit$greetings"){
+           //send greetings message
+           callSendAPI(sender_psid,'Hi there! This bot is created by Hary Pham. Watch more videos on HaryPhamDev Channel!');
+       }
+       if(entityChosen === "wit$thanks"){
+           //send thanks message
+           callSendAPI(sender_psid,`You 're welcome!`);
+       }
+        if(entityChosen === "wit$bye"){
+            //send bye message
+            callSendAPI(sender_psid,'bye-bye!');
+        }
+    }
+}
 
 // let callSendAPIWithTemplate = (sender_psid) => {
 //     // document fb message template
@@ -245,47 +198,6 @@ function callSendAPI(sender_psid, response) {
 //         }
 //     });
 // };
-
-function handleMessage(sender_psid, received_message) {
-
-    let response;
-  
-    // Check if the message contains text
-    if (received_message.text) {    
-  
-      // Create the payload for a basic text message
-      response = {
-        "text": `You sent the message: "${received_message.text}". Now send me an image!`
-      }
-    }  
-    
-    // Sends the response message
-    callSendAPI(sender_psid, response);    
-  }
-
-  function callSendAPI(sender_psid, response) {
-    // Construct the message body
-    let request_body = {
-      "recipient": {
-        "id": sender_psid
-      },
-      "message": response
-    }
-  
-    // Send the HTTP request to the Messenger Platform
-    request({
-      "uri": "https://graph.facebook.com/v2.6/me/messages",
-      "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-      "method": "POST",
-      "json": request_body
-    }, (err, res, body) => {
-      if (!err) {
-        console.log('message sent!')
-      } else {
-        console.error("Unable to send message:" + err);
-      }
-    }); 
-  }
 
 module.exports = {
   postWebhook: postWebhook,
