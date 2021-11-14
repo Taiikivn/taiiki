b490ddb4e6afeb7c267e4330f53daac24b1310db
thuat <tk30ofmusic@gmail.com>
2021-11-14 19:15:58 +0700

    update

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -271,8 +271,21 @@ function handleMessage(sender_psid, received_message) {
       },
       "message": response
     }
+  
+    // Send the HTTP request to the Messenger Platform
+    request({
+      "uri": "https://graph.facebook.com/v2.6/me/messages",
+      "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+      "method": "POST",
+      "json": request_body
+    }, (err, res, body) => {
+      if (!err) {
+        console.log('message sent!')
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }); 
   }
-
 
 module.exports = {
   postWebhook: postWebhook,
